76fbfb13ee53978aa02e3f7d9dafc1029025e112
Guilherme Guimaraes <technestowl@gmail.com>
2024-09-26 09:48:13 -0400

    Generating random number. Checking user input 'guess' value.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            titleLbl = new Label();
+            title2Lbl = new Label();
+            playButton = new Button();
+            playerGuessLbl = new Label();
+            incorrectGuessCount = new Label();
+            textBox1 = new TextBox();
+            incorrectGuessLabel = new Label();
+            correctGuessLabel = new Label();
+            correctGuessCount = new Label();
+            SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            titleLbl.AutoSize = true;
+            titleLbl.Font = new Font("Segoe UI Historic", 16.2782612F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point, 0);
+            titleLbl.Location = new Point(343, 53);
+            titleLbl.Name = "titleLbl";
+            titleLbl.Size = new Size(223, 36);
+            titleLbl.TabIndex = 0;
+            titleLbl.Text = "Guess A Number";
+            // 
+            // title2Lbl
+            // 
+            title2Lbl.AutoSize = true;
+            title2Lbl.Location = new Point(416, 101);
+            title2Lbl.Name = "title2Lbl";
+            title2Lbl.Size = new Size(55, 20);
+            title2Lbl.TabIndex = 1;
+            title2Lbl.Text = "1 - 100";
+            // 
+            // playButton
+            // 
+            playButton.Location = new Point(550, 246);
+            playButton.Name = "playButton";
+            playButton.Size = new Size(90, 28);
+            playButton.TabIndex = 2;
+            playButton.Text = "Play Game";
+            playButton.UseVisualStyleBackColor = true;
+            playButton.Click += playButton_Click;
+            // 
+            // playerGuessLbl
+            // 
+            playerGuessLbl.AutoSize = true;
+            playerGuessLbl.Font = new Font("Segoe UI", 8.765218F, FontStyle.Underline, GraphicsUnit.Point, 0);
+            playerGuessLbl.Location = new Point(132, 170);
+            playerGuessLbl.Name = "playerGuessLbl";
+            playerGuessLbl.Size = new Size(112, 20);
+            playerGuessLbl.TabIndex = 3;
+            playerGuessLbl.Text = "Guess Attempts";
+            // 
+            // incorrectGuessCount
+            // 
+            incorrectGuessCount.AutoSize = true;
+            incorrectGuessCount.Location = new Point(109, 254);
+            incorrectGuessCount.Name = "incorrectGuessCount";
+            incorrectGuessCount.Size = new Size(17, 20);
+            incorrectGuessCount.TabIndex = 4;
+            incorrectGuessCount.Text = "0";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(377, 246);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "1 -100";
+            textBox1.Size = new Size(94, 26);
+            textBox1.TabIndex = 5;
+            // 
+            // incorrectGuessLabel
+            // 
+            incorrectGuessLabel.AutoSize = true;
+            incorrectGuessLabel.Location = new Point(72, 211);
+            incorrectGuessLabel.Name = "incorrectGuessLabel";
+            incorrectGuessLabel.Size = new Size(109, 20);
+            incorrectGuessLabel.TabIndex = 6;
+            incorrectGuessLabel.Text = "Incorrect Guess";
+            // 
+            // correctGuessLabel
+            // 
+            correctGuessLabel.AutoSize = true;
+            correctGuessLabel.Location = new Point(205, 211);
+            correctGuessLabel.Name = "correctGuessLabel";
+            correctGuessLabel.Size = new Size(99, 20);
+            correctGuessLabel.TabIndex = 7;
+            correctGuessLabel.Text = "Correct Guess";
+            // 
+            // correctGuessCount
+            // 
+            correctGuessCount.AutoSize = true;
+            correctGuessCount.Location = new Point(240, 254);
+            correctGuessCount.Name = "correctGuessCount";
+            correctGuessCount.Size = new Size(17, 20);
+            correctGuessCount.TabIndex = 8;
+            correctGuessCount.Text = "0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(911, 762);
+            Controls.Add(correctGuessCount);
+            Controls.Add(correctGuessLabel);
+            Controls.Add(incorrectGuessLabel);
+            Controls.Add(textBox1);
+            Controls.Add(incorrectGuessCount);
+            Controls.Add(playerGuessLbl);
+            Controls.Add(playButton);
+            Controls.Add(title2Lbl);
+            Controls.Add(titleLbl);
+            Name = "Form1";
+            Text = "C# Number Guessing Game";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label titleLbl;
+        private Label title2Lbl;
+        private Button playButton;
+        private Label playerGuessLbl;
+        private Label incorrectGuessCount;
+        private TextBox textBox1;
+        private Label incorrectGuessLabel;
+        private Label correctGuessLabel;
+        private Label correctGuessCount;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,16 @@
+/*********************************************************
+
+   Gill Guimaraes
+
+    Mini Project Part II - September 26, 2024
+
+    Number Guessing Game
+
+   I wrote this code myself...
+   I did not use AI or copy code from Google or another student
+
+ *********************************************************/
+
 namespace NumberGuessingGame
 {
     public partial class Form1 : Form
@@ -5,6 +18,41 @@ namespace NumberGuessingGame
         public Form1()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+        int guessCounter = 0;
+
+
+        //generate number 1 - 100
+        int generateRandomNum()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 101);
+            return randomNumber;
+        }
+
+
+        //compare values of the params
+        void checkGuess(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                MessageBox.Show("Your guess is too high");
+            }
+            else if (num1 < num2)
+            {
+                MessageBox.Show("Your guess is too low");
+            }
+            else if (num1 == num2)
+            {
+                MessageBox.Show("You guessed right, Congrats!");
+            }
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+
         }
     }
 }
